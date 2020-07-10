e451fcb493010181025b71956ef00f8e5814be68
Shaik Sadath Basha <32085146+shaik16@users.noreply.github.com>
2020-07-10 18:12:20 +0530

    Update average_pair.js

--- 100% OPTIONAL Challenges/average_pair.js
+++ 100% OPTIONAL Challenges/average_pair.js
@@ -11,7 +11,7 @@
 //      averagePair([1,2,3], 2.5); // true
 //      averagePair([1,3,3,5,6,7,10,12,19], 8); // true
 //      averagePair([-1,0,3,4,5,6], 4.1); // false
-//      averagePair([], 4); // false
+averagePair([], 4); // false
 
 function averagePair(arr, num){
     let start = 0;
